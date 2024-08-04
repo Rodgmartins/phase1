b5357103f7e34170eb2bbaee13bd0f8ac8192458
Rodigo G Martins <mrgobetti0@gmail.com>
2024-08-04 12:43:19 -0300

    refactor(linearSearch): corrigindo problema do codigo

--- codefolder/linearSearch.py
+++ codefolder/linearSearch.py
@@ -2,5 +2,4 @@ def linearSearch(lista, key):
     for i in range(len(lista)):
         if lista[i] == key:
             return f"O número {lista[i]} encontra-se na posição {i} da lista"
-        else:
-            return -1
+    return -1
