6bc406b2f34880dc95e69b92b9a673bbf5254d13
Rodigo G Martins <mrgobetti0@gmail.com>
2024-08-05 19:25:46 -0300

    feat(bubbleSort): criando codigo

---
+++ codefolder/bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(lista):
+    for n in range(len(lista),0,-1):
+        shift = False
+        for i n range(0, n-1):
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+                shift = True
+        if not shit:
+            break
